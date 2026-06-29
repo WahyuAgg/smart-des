23ae194de72dfb6900506f21f83b685652f88127
WahyuAgg <wahyuagungbarokah@mail.ugm.ac.id>
2026-06-30 02:02:32 +0700

    feat: seeder pekerjaan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            // UserSeeder::class,
+            // AlamatSeeder::class,
+            // KkSeeder::class,
+            // PendidikanSeeder::class,
+            PekerjaanSeeder::class,
         ]);
+
     }
+
 }
